59894edf39bfe7d083b13054e4f5e24ed6ac88f6
Hector <hector5991@gmail.com>
2019-02-13 11:57:02 +0100

    Make an MainController
    You should put your rutes here

---
+++ Daw/src/main/java/com/urjc/daw/Controller/MainController.java
@@ -0,0 +1,18 @@
+package com.urjc.daw.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(path = "/")
+    public String showIndex(Model model){
+        return "index";
+    }
+
+    @GetMapping(path = "/mainPage")
+    public String showPrueba(Model model){
+        return "MainPage";
+    }
+}

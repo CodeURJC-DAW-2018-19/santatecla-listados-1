7d40402f72efdbf377e84f34fc024eb1b3588842
Hector <hector5991@gmail.com>
2019-02-13 15:40:32 +0100

    ItemRepository

--- Daw/src/main/java/com/urjc/daw/Models/Item/ItemRepository.java
+++ Daw/src/main/java/com/urjc/daw/Models/Item/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.urjc.daw.Models.Item;
 
 import com.urjc.daw.Models.Item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -8,6 +9,6 @@ import org.springframework.data.repository.CrudRepository;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface ItemRepository extends CrudRepository<Item, Integer> {
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 }

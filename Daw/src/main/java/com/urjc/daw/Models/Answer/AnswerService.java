fab2148e1b40773e59f2ba0388799e295ff2c077
sandra1012 <s.canadas.2016@alumnos.urjc.es>
2019-02-19 11:11:04 +0100

    Update MainController

--- Daw/src/main/java/com/urjc/daw/Models/Answer/AnswerService.java
+++ Daw/src/main/java/com/urjc/daw/Models/Answer/AnswerService.java
@@ -1,10 +1,12 @@
 package com.urjc.daw.Models.Answer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AnswerService {
 
     @Autowired
--- Daw/src/main/java/com/urjc/daw/ViewsControllers/MainController.java
+++ Daw/src/main/java/com/urjc/daw/ViewsControllers/MainController.java
@@ -5,6 +5,9 @@ import com.urjc.daw.Models.Concept.ConceptService;
 import com.urjc.daw.Models.Item.ItemService;
 import com.urjc.daw.Models.Lessons.Lesson;
 import com.urjc.daw.Models.Lessons.LessonService;
+import com.urjc.daw.Models.Question.Question;
+import com.urjc.daw.Models.Question.QuestionService;
+import com.urjc.daw.Models.Answer.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +28,13 @@ public class MainController {
 
     @Autowired
     ItemService itemService;
+
+    @Autowired
+    QuestionService questionService;
+
+    @Autowired
+    AnswerService answerService;
+
 
 
     @RequestMapping(path = "/")
@@ -64,5 +74,13 @@ public class MainController {
         model.addAttribute("itemsCorrect", itemService.findItemByState(true));
         model.addAttribute("itemsIncorrect", itemService.findItemByState(false));
         return "ConceptView/ConceptTeacher/TeacherConcept_View";
+    }
+
+    @RequestMapping(path = "/StudentConcept")
+    public String showStudentConcept(Model model) {
+        model.addAttribute("question", questionService.findAll());
+        model.addAttribute("answer", answerService.findAll());
+        model.addAttribute("items", itemService.findAll());
+        return "ConceptView/StudentConceptView";
     }
 }

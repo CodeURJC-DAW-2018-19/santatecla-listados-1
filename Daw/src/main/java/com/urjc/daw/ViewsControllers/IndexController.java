bd80314e7acc95b35eab1fcb4252a9e81293f1f3
Hector <hector5991@gmail.com>
2019-02-23 12:56:13 +0100

    Add diagram function

--- Daw/src/main/java/com/urjc/daw/ViewsControllers/IndexController.java
+++ Daw/src/main/java/com/urjc/daw/ViewsControllers/IndexController.java
@@ -14,15 +14,6 @@ public class IndexController {
 
     @RequestMapping(path="/login") // Map ONLY GET Requests
     public String login () {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        //User n = new User(name,"",email,"");
-        //n.setName(name);
-        //n.setEmail(email);
-        //System.out.println(name);
-        //System.out.println(email);
-        //userRepository.save(n);
         return "Login";
     }
 

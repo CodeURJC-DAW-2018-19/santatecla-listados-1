3ff9d02fcabc6d6eb7af30abf0c898667f7f90b9
Hector <hector5991@gmail.com>
2019-03-09 13:34:54 +0100

    SafeDelete and SafeCreate

--- Daw/src/main/java/com/urjc/daw/api_rest/CheckIfCreate.java
+++ Daw/src/main/java/com/urjc/daw/api_rest/CheckIfCreate.java
@@ -2,15 +2,33 @@ package com.urjc.daw.api_rest;
 
 
 import com.urjc.daw.models.answer.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 public class CheckIfCreate <T>{
-
     public ResponseEntity<T> checkIfExist(Optional<T> t){
         if (t.isPresent()) {
+            return new ResponseEntity<>(t.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<T> safeDelete(Optional<T> t, JpaRepository<T,Long> service){
+        if (t.isPresent()) {
+            service.delete(t.get());
+            return new ResponseEntity<>(t.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<T> safeCreate(Optional<T> t, JpaRepository<T,Long> service){
+        if (t.isPresent()) {
+            service.save(t.get());
             return new ResponseEntity<>(t.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);

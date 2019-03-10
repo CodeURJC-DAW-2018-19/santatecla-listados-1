d82300a61dbec67b3d5389669aa580f12bdde228
Hector <hector5991@gmail.com>
2019-03-10 18:35:54 +0100

    Docker config

--- Daw/syncWithDB.sh
+++ Daw/syncWithDB.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
-while ! mysql --protocol TCP -u "daw_grupo9" -p "URJCDAWGrupo9" -e"show databases;" > /dev/null 2>&1; do
-	sleep 3
-	echo 'app failed'
+until nc -z -v -w30 daw_grupo9 3306
+do
+  echo "Waiting for database connection..."
+  sleep 5
 done
 java -jar target/daw-0.0.1-SNAPSHOT.jar

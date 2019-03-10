a95c2c53490f7170cc7b736718cf07fd30b190e5
Ernesto Baltasar Nve Obono <nveobono1@gmail.com>
2019-03-10 05:14:00 +0100

    uploading file
    fix some errors

--- Daw/src/main/java/com/urjc/daw/api_rest/ConceptRest.java
+++ Daw/src/main/java/com/urjc/daw/api_rest/ConceptRest.java
@@ -14,11 +14,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
 
@@ -57,11 +60,7 @@ public class ConceptRest extends OperationsRest<Concept> {
 
     @PostMapping("/lesson/{idLesson}")
     @JsonView(ConceptDetails.class)
-    public ResponseEntity<Concept> saveConcept(@RequestBody Concept concept, @RequestParam("file") MultipartFile multipartFile, @PathVariable long idLesson) throws IOException {
-        String filename = uploadFileService.copy(multipartFile);
-        String fileUrl = ServletUriComponentsBuilder.fromCurrentContextPath().path("/uploads/").path(filename).toUriString();
-        concept.setPicture(fileUrl);
-
+    public ResponseEntity<Concept> saveConcept(@RequestBody Concept concept, @PathVariable long idLesson){
         Optional<Lesson> optionalLesson = lessonService.findOne(idLesson);
         if(optionalLesson.isPresent()){
             Lesson lesson = optionalLesson.get();
@@ -77,6 +76,16 @@ public class ConceptRest extends OperationsRest<Concept> {
         }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
 
+    @RequestMapping(value = "/uploads", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> uploadsImage(@RequestParam("file") MultipartFile file) throws IOException{
+        File fileConcept = new File("C://Temp//uploads", file.getOriginalFilename());
+        fileConcept.createNewFile();
+        FileOutputStream fout = new FileOutputStream(fileConcept);
+        fout.write(file.getBytes());
+        fout.close();
+        //ResponseEntity<> image = new
+        return new ResponseEntity<>("File updated ", HttpStatus.OK);
     }
 }

eec3a13d5f143421b998ec109e626c401a51fc1f
Hector <hector5991@gmail.com>
2019-03-19 10:16:05 +0100

    CreateService

--- DawFase4/src/app/model/model.component.ts
+++ DawFase4/src/app/model/model.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-model',
-  templateUrl: './model.component.html',
-  styleUrls: ['./model.component.css']
-})
 export class ModelComponent implements OnInit {
 
   constructor() { }

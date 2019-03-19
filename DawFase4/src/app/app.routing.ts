26f37879328b288b97956d66183fbdb4d1ec47f0
sandra1012 <46897050+sandra1012@users.noreply.github.com>
2019-03-19 18:57:52 +0100

    StudentConceptView started

--- DawFase4/src/app/app.routing.ts
+++ DawFase4/src/app/app.routing.ts
@@ -3,14 +3,14 @@ import {LoginComponent} from './login/login.component';
 import {MainpageComponent} from './mainpage/mainpage.component';
 
 import { TeacherConceptViewComponent } from './teacher-concept-view/teacher-concept-view.component';
-
+import {StudentConceptViewComponent} from './student-concept-view/student-concept-view.component';
 
 const appRoutes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index', component: LoginComponent},
   { path: 'main', component: MainpageComponent},
-  { path: 'teacherConceptView', component: TeacherConceptViewComponent }
-
+  { path: 'teacherConceptView', component: TeacherConceptViewComponent },
+  { path: 'studentConceptView', component: StudentConceptViewComponent}
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);

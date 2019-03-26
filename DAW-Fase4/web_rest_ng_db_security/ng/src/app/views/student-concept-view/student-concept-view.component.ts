435826150eece4f6c1bb8b5bd9b39bd776405d75
sandra1012 <46897050+sandra1012@users.noreply.github.com>
2019-03-26 20:23:20 +0100

    Update studentConceptView

--- DAW-Fase4/web_rest_ng_db_security/ng/src/app/views/student-concept-view/student-concept-view.component.ts
+++ DAW-Fase4/web_rest_ng_db_security/ng/src/app/views/student-concept-view/student-concept-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {MatDialog, MatDialogRef} from "@angular/material";
 
 @Component({
     selector: 'app-student-concept-view',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./student-concept-view.component.css']
 })
 export class StudentConceptViewComponent implements OnInit {
+    @ViewChild('diagramDialog') diagramDialog: TemplateRef<any>;
+    dialogRef: MatDialogRef<any, any>;
+    @ViewChild('questionDialog') questionDialog: TemplateRef<any>;
+    questionRef: MatDialogRef<any, any>;
 
-    constructor() { }
+    constructor(public dialog: MatDialog) { }
 
     ngOnInit() {
+    }
+    openDiagramDialog() {
+        this.dialogRef = this.dialog.open(this.diagramDialog, {
+            width: '50%',
+            height: '50%',
+        });
+    }
+
+    openQuestionDialog() {
+        this.dialogRef = this.dialog.open(this.questionDialog, {
+            width: '50%',
+            height: '50%',
+        });
     }
 
 }
